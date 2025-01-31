0f208eb6a6b38905a1971a64232c78046b2ab329
byTheInK <un.known.river.person@gmail.com>
2025-01-31 16:33:37 +0300

    Arch setted up

---
+++ arch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Setting up..."
+sleep 1
+sudo pacman -S --needed git base-devel
+
+if ! command -v yay &> /dev/null
+then
+    echo "Yay not found installing"
+    sleep 1
+    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    echo "Installed Yay"
+    sleep 1
+fi
+
+_done=false
+
+while [$_done]; do
+    echo "[1]: qemu-full <- Recommended "
+    echo "[2]: qemu-base"
+    echo "[3]: qemu-desktop"
+
+    echo -n ": " 
+    read _qemu
+
+    if [$_qemu = "1"]; then
+        
+    fi
+done
