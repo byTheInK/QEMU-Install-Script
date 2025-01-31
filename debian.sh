c06bf9f065a2ce83bfd4b698de44430551f58b57
byTheInK <un.known.river.person@gmail.com>
2025-01-31 16:17:04 +0300

    Fixed Qemu name

--- debian.sh
+++ debian.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "Updating system..."
 sudo apt update &> /dev/null
-echo "Updated the system"
+echo "System updated."
 sleep 1
 
-echo "Installing Qemu, Libvirt, KVM and  Virt-manager"
-sudo apt install qemu qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager &> /dev/null
-echo "Installed required packages sucsessfully"
+echo "Installing Qemu, Libvirt, KVM, and Virt-manager..."
+sudo apt install -y qemu-system-x86 libvirt-daemon-system libvirt-clients bridge-utils virt-manager &> /dev/null
+echo "Required packages installed successfully."
